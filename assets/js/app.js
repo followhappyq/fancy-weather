3c03658baeb5b6b022d4ab22a52d0107b7e74887
Ivan Dubovtsov <followhappyq@gmail.com>
2020-04-06 18:09:45 +0200

    input styles added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,17 @@
 //Dom elements
 const body = document.querySelector("body");
 
-const btn = document.querySelector(".talk");
-const content = document.querySelector(".content");
+/** Voice recognition DOM elements START*/
+const btn = document.querySelector(".voice-recognition .talk");
+const voiceInput = document.querySelector(".voice-recognition .result");
+/** Voice recognition DOM elements END */
 
 async function getLinkToImage() {
   const url = `https://api.unsplash.com/photos/random?query=morning&client_id=${properties.unsplash}`;
   const res = await fetch(url);
   if (res.ok) {
     const data = await res.json();
-    body.style.backgroundImage = `url(${data.urls.full})`;
+    body.style.backgroundImage = `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3) ), url('${data.urls.full}')`;
   } else {
     console.log(res.status);
   }
@@ -27,6 +29,11 @@ recognition.onresult = (event) => {
   const current = event.resultIndex;
   const transcript = event.results[current][0].transcript;
   console.log(transcript);
+  saveRecognitionToInput(transcript);
+};
+
+const saveRecognitionToInput = (transcript) => {
+  voiceInput.value = transcript;
 };
 
 //Add listener to the button
